c3cc5ecade57b2486dd1027535bd5637575f1803
amirprync <60889238+amirprync@users.noreply.github.com>
2024-07-24 19:44:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -346,7 +346,7 @@ def generate_pdf_prestamo_entre_clientes(mes, dia, cliente, interes, prestamista
     return pdf.output(dest='S').encode('latin1')
 
 # Función para generar el PDF para PRESTAMO ENTRE CLIENTES T-BILLS
-def generate_pdf_prestamo_entre_clientes_tbills(mes, dia, cliente, interes, prestamista, comitente_prestamista, depositante_prestamista, tomador, comitente_tomador, depositante_tomador, especie, codigo_especie, valor_nominal, tasa_anual, plazo, cuenta_bancaria, cuit_prestamista, domicilio_prestamista, cuit_tomador, domicilio_tomador):
+def generate_pdf_prestamo_entre_clientes_tbills(mes, dia, cliente, interes, prestamista, comitente_prestamista, depositante_prestamista, tomador, comitente_tomador, depositante_tomador, especie, codigo_especie, valor_nominal, valor_nominal_texto, tasa_anual, plazo, plazo_texto, cuenta_bancaria, cuit_prestamista, domicilio_prestamista, cuit_tomador, domicilio_tomador):
     pdf = PDF()
     pdf.add_page()
     pdf.set_auto_page_break(auto=True, margin=15)
