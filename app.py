dbbdbd3f8fcb087e455f9d3cf04cbef0da197298
amirprync <60889238+amirprync@users.noreply.github.com>
2024-08-09 10:14:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -534,6 +534,10 @@ if st.button("Generar PDF"):
             elif tipo_prestamo == "PRESTAMO ENTRE CLIENTES T-BILLS":
                 pdf_data = generate_pdf_prestamo_entre_clientes_tbills(mes, dia, cliente, interes, prestamista, comitente_prestamista, depositante_prestamista, tomador, comitente_tomador, depositante_tomador, especie, codigo_especie, valor_nominal, valor_nominal_texto, tasa_anual, plazo, plazo_texto, cuenta_bancaria, cuit_prestamista, domicilio_prestamista, cuit_tomador, domicilio_tomador)
             
+            # Convertir pdf_data a bytes si es un bytearray
+            if isinstance(pdf_data, bytearray):
+                pdf_data = bytes(pdf_data)
+            
             # Enviar el correo automáticamente
             enviar_email(pdf_data, "oferta_prestamo.pdf")
             
